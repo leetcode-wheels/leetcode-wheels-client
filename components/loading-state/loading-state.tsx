8f0d31eaa528c55ba37e03cf761943bcba52f595
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2022-09-20 12:36:36 -0400

    feat(loading-state): fix to top

--- components/loading-state/loading-state.tsx
+++ components/loading-state/loading-state.tsx
@@ -7,21 +7,23 @@ export type LoadingStateProps = {
 
 const LoadingState: React.FC<LoadingStateProps> = ({ show = false }) => {
   return (
-    <Transition
-      as="div"
-      show={show}
-      appear
-      enter="transition-opacity duration-200"
-      enterFrom="opacity-0"
-      enterTo="opacity-100"
-      leave="transition-opacity duration-200"
-      leaveFrom="opacity-100"
-      leaveTo="opacity-0"
-    >
-      <div className="backdrop-blur-sm h-screen w-full fixed inset-0 bg-transparent flex flex-col items-center justify-center">
-        <Ping size={65} speed={2} color="gray" />
-      </div>
-    </Transition>
+    <div className="fixed z-50">
+      <Transition
+        as="div"
+        show={show}
+        appear
+        enter="transition-opacity duration-200"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="transition-opacity duration-200"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+      >
+        <div className="backdrop-blur-sm h-screen w-full fixed inset-0 bg-transparent flex flex-col items-center justify-center">
+          <Ping size={65} speed={2} color="gray" />
+        </div>
+      </Transition>
+    </div>
   )
 }
 
